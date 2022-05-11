5f26631b9a9b10887584cac3fa3ed40859d57e97
mykongee <mikehongde@gmail.com>
2022-05-10 18:18:45 -0700

    Added Box component with useFrame hook to animate rotation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { Canvas } from "@react-three/fiber";
+import { useRef } from 'react';
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Box() {
+  const ref = useRef();
+
+  useFrame((state) => {
+    ref.current.rotation.y = Math.PI * Math.sin(state.clock.elapsedTime);
+    ref.current.rotation.x = Math.cos(state.clock.elapsedTime);
+  })
+
+  return (
+    <mesh ref={ref}>
+      <boxGeometry />
+      <meshBasicMaterial color="red" />
+    </mesh>
+  )
+}
+
 
 export default function App() {
   return (
     <Canvas>
-      <mesh position={[0, 0, 0]}>
-        <boxGeometry />
-        <meshBasicMaterial color="red" />
-      </mesh>
+      <Box />
     </Canvas>
   )
 }
